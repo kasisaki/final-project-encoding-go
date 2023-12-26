461b1d4da5d31888bb980dfde3a2c092079bb9ae
mutaev <i.mutaev@gmail.com>
2023-12-27 02:51:32 +0300

    realise (y *YAMLData) Encoding() realise (j *JSONData) Encoding()

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -1,7 +1,10 @@
 package encoding
 
 import (
+	"encoding/json"
 	"github.com/Yandex-Practicum/final-project-encoding-go/models"
+	"gopkg.in/yaml.v3"
+	"os"
 )
 
 // JSONData тип для перекодирования из JSON в YAML
@@ -25,16 +28,56 @@ type MyEncoder interface {
 
 // Encoding перекодирует файл из JSON в YAML
 func (j *JSONData) Encoding() error {
-	// ниже реализуйте метод
-	// ...
+	// Чтение данных в JSON формате из файла
+	jsonData, err := os.ReadFile(j.FileInput)
+	if err != nil {
+		return err
+	}
+
+	// Десериализация данных JSON в структуру
+	var dockerCompose models.DockerCompose
+	if err := json.Unmarshal(jsonData, &dockerCompose); err != nil {
+		return err
+	}
+
+	// Сериализация структуры в YAML
+	yamlData, err := yaml.Marshal(dockerCompose)
+	if err != nil {
+		return err
+	}
+
+	// Запись JSON в файл
+	if err := os.WriteFile(j.FileOutput, yamlData, 0644); err != nil {
+		return err
+	}
 
 	return nil
 }
 
 // Encoding перекодирует файл из YAML в JSON
 func (y *YAMLData) Encoding() error {
-	// Ниже реализуйте метод
-	// ...
+	// Чтение данных из файла
+	yamlData, err := os.ReadFile(y.FileInput)
+	if err != nil {
+		return err
+	}
+
+	// Десериализация данных YAML в структуру
+	var dockerCompose models.DockerCompose
+	if err := yaml.Unmarshal(yamlData, &dockerCompose); err != nil {
+		return err
+	}
+
+	// Сериализация структуры в JSON
+	jsonData, err := json.Marshal(dockerCompose)
+	if err != nil {
+		return err
+	}
+
+	// Запись JSON в файл
+	if err := os.WriteFile(y.FileOutput, jsonData, 0644); err != nil {
+		return err
+	}
 
 	return nil
 }
